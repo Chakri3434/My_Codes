fee35ab969ea500147ea93d6051f08958c8760af
Chakri_34 <120355431+Chakri3434@users.noreply.github.com>
2024-05-09 11:50:14 +0530

    Create Tower-of-Hanoi.py

---
+++ Basic programs/Recursions/Tower-of-Hanoi.py
@@ -0,0 +1,18 @@
+# move 1 disc at a time
+# no larger disc above smaller
+# only top discs can be moved
+# n is number of discs
+# a is initial position
+# b is auxilary position
+# c is final destination
+
+def toh(n,a,b,c):
+  if n==1:
+    print('move disc 1 from A to C')
+    return
+  toh(n-1,a,c,b)
+  print('move disc '+str(n)+' from A to C')
+  toh(n-1,b,a,c)
+for i in range(int(input())):
+  n=int(input())
+  toh(n,a,b,c)
