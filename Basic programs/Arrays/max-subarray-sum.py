90dd2a38c513fd056421a3cacf06d6c4e667ff17
Chakri_34 <120355431+Chakri3434@users.noreply.github.com>
2024-05-16 15:47:12 +0530

    Create max-subarray-sum.py

---
+++ Basic programs/Arrays/max-subarray-sum.py
@@ -0,0 +1,18 @@
+# naive : O(n^2)
+def f(l):
+    res=l[0]
+    for i in range(len(l)):
+        curr=0
+        for j in range(i,len(l)):
+            curr+=l[i]
+            res=max(res,curr)
+    return res
+
+# efficient : O(n)
+def f(l):
+    res=l[0]
+    maxEnding=l[0]
+    for i in range(1,len(l)):
+        maxEnding=max(maxEnding+l[i],l[i])
+        res=max(res,maxEnding)
+    return res
