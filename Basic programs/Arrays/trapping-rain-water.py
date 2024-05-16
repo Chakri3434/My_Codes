785ac83cde74ad5fa19417120760262a2e9cfe9d
Chakri_34 <120355431+Chakri3434@users.noreply.github.com>
2024-05-16 11:26:30 +0530

    Update trapping-rain-water.py

--- Basic programs/Arrays/trapping-rain-water.py
+++ Basic programs/Arrays/trapping-rain-water.py
@@ -3,16 +3,18 @@ def f(a):
     
     lmax=[a[0]]
     for i in range(1,len(a)):
-        lmax[i]=max(a[i],lmax[i-1])
-        
-    rmax=[a[-1]]
-    j=len(l)-2
+        lmax.append(max(a[i],lmax[i-1]))
+    
+    rmax=[0 for i in range(len(a))]
+    j=len(a)-2
+    rmax[-1]=a[-1]
     while j>=0:
-        rmax=max(a[j],rmax[j+1])
+        rmax[j]=max(a[j],rmax[j+1])
         j-=1
-      
-    for i in range(1,len(l)-1):
-        res+=min(lmax[i],rmax[i])-a[i]
+    
+    for i in range(1,len(a)-1):
+        res+=(min(lmax[i],rmax[i])-a[i])
+    
     return res
 l=list(map(int,input().split()))
 print(f(l))
