41cba4185bccb59da4580dd9206387bda3022738
Chakri_34 <120355431+Chakri3434@users.noreply.github.com>
2024-05-17 12:33:17 +0530

    Update majority-element.py

--- Basic programs/Arrays/majority-element.py
+++ Basic programs/Arrays/majority-element.py
@@ -11,3 +11,24 @@ def f(l):
         else:
             d[l[i]]=1 
     return -1
+
+#Moore's Voting Algorithm
+
+def f(l):
+    res=0 
+    c=1 
+    for i in range(1,len(l)):
+        if l[res]==l[i]:
+            c+=1 
+        else:
+            c-=1 
+        if c==0:
+            res=i
+            c=1 
+    c=0 
+    for i in range(len(l)):
+        if l[i]==l[res]:
+            c+=1 
+    if c<=len(l)//2:
+        res=-1
+    return res
