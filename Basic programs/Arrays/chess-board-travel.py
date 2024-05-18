0c296b8bfb900b690c7a5971ae6da5c1a4c7521c
Chakri_34 <120355431+Chakri3434@users.noreply.github.com>
2024-05-18 17:11:35 +0530

    Create chess-board-travel.py

---
+++ Basic programs/Arrays/chess-board-travel.py
@@ -0,0 +1,43 @@
+def f():
+    d={'a':0,'b':1,'c':2,'d':3,'e':4,'f':5,'g':6,'h':7}
+    d1={v:k for k,v in d.items()}
+    res='h8'
+    k=3
+    for i in range(k):
+        l=list(map(int,input().split()))
+        
+        for i in range(len(l)):
+            for j in range(l[i][0]):
+                if l[i][1]=='l':
+                    if res[0]!='a':
+                        res[0]=d1[d[int(res[0])]-1]
+                    else:
+                        return -1
+                elif l[i][1]=='r':
+                    if res[0]!='h':
+                        res[0]=d1[d[int(res[0])]+1]
+                    else:
+                        return -1
+                elif l[i][1]=='u':
+                    if res[1]!='8':
+                        res[1]=int(res[1])+1 
+                    else:
+                        return -1
+                elif l[i][1]=='d':
+                    if res[1]!='1':
+                        res[1]=int(res[1])-1
+                    else:
+                        return -1
+        return res
+                    
+print(f())                    
+    
+    
+# // h8
+# // 3 
+# // 2l 
+# // 2d 
+# // 1r
+
+# //g6 
+
