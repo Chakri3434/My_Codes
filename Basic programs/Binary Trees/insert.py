9737cda55eb0ee85f32151c4bd51478d9e690f0b
Chakri_34 <120355431+Chakri3434@users.noreply.github.com>
2024-05-19 15:19:39 +0530

    Create insert.py

---
+++ Basic programs/Binary Trees/insert.py
@@ -0,0 +1,30 @@
+class TreeNode:
+    
+    def __init__(self,value):
+        self.left=None
+        self.right=None
+        self.value=value
+        
+    def insert(self,value):
+        if value<self.value:
+            if self.left is None:
+                self.left=TreeNode(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right=TreeNode(value)
+            else:
+                self.right.insert(value)
+     
+tree=TreeNode(4)
+tree.insert(5)
+tree.insert(1)
+tree.insert(9)
+tree.insert(11)
+tree.insert(29)
+tree.insert(8)
+tree.insert(6)
+tree.insert(14)
+tree.insert(10)
+
