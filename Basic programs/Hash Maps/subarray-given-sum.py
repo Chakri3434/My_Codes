c2aeaf0773ad7c8edc9de16e7bcbdeca7c5a3202
Chakri_34 <120355431+Chakri3434@users.noreply.github.com>
2024-05-23 10:29:56 +0530

    Update subarray-given-sum.py

--- Basic programs/Hash Maps/subarray-given-sum.py
+++ Basic programs/Hash Maps/subarray-given-sum.py
@@ -1,13 +1,13 @@
 def f(l,k):
-    s=[]
+    s=set()
     preSum=0
     for i in range(len(l)):
         preSum+=l[i]
-        if preSum-k in s:
-            return True
         if preSum==k:
             return True
-        s.append(preSum)
+        if (preSum-k) in s:
+            return True
+        s.add(preSum)
     return False
 l=[5,8,6,13,3,-1]
 print(f(l,22))
