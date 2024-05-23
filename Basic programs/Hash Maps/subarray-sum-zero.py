3cd9e0766a32a057af097a8fcd5b1588214d2de0
Chakri_34 <120355431+Chakri3434@users.noreply.github.com>
2024-05-23 10:27:11 +0530

    Update subarray-sum-zero.py

--- Basic programs/Hash Maps/subarray-sum-zero.py
+++ Basic programs/Hash Maps/subarray-sum-zero.py
@@ -1,13 +1,13 @@
 def f(l):
-    s=[]
+    s=set()
     preSum=0
     for i in range(len(l)):
         preSum+=l[i]
-        if preSum in s:
-            return True
         if preSum==0:
             return True
-        s.append(preSum)
+        if preSum in s:
+            return True
+        s.add(preSum)
     return False
 l=[1,4,13,-3,-10,5]
 print(f(l))
