b3faab60d32b94c76a6a16598b213032d129aecb
Chakri_34 <120355431+Chakri3434@users.noreply.github.com>
2024-05-24 11:28:55 +0530

    Update palindrome.py

--- Basic programs/Strings/palindrome.py
+++ Basic programs/Strings/palindrome.py
@@ -11,3 +11,15 @@ def f(l):
     return True
 l='grouorg'
 print(f(l))
+
+#using recursions
+
+def pal(n):
+  p=n.lower()
+  if p=='':
+    return True
+  elif p[0]==p[-1]:
+    return pal(p[1:len(p)-1])
+  else:
+    return False
+print(pal(input()))
