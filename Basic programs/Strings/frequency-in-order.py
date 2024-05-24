346351aae86fa58fbb7fc9fbdeaa43a7606d0848
Chakri_34 <120355431+Chakri3434@users.noreply.github.com>
2024-05-24 11:18:23 +0530

    Update frequency-in-order.py

--- Basic programs/Strings/frequency-in-order.py
+++ Basic programs/Strings/frequency-in-order.py
@@ -1,3 +1,5 @@
+#using strings
+
 def f(l):
     count=[0 for i in range(26)]
     for i in range(len(l)):
@@ -5,5 +7,20 @@ def f(l):
     for i in range(len(count)):
         if count[i]!=0:
             print(chr(i + ord('a')),count[i])
+l='geeksforgeeks'
+f(l)
+
+#using hashmap
+
+def f(l):
+    d={}
+    for i in range(len(l)):
+        if l[i] in d:
+            d[l[i]]+=1 
+        else:
+            d[l[i]]=1 
+    q=[(k,v) for k,v in d.items()]
+    q.sort(key= lambda x:x[0])
+    print(q)
 l='geeksforgeeks'
 f(l)
