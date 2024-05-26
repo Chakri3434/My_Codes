ae787d9d39218d2233f15caef24de87d0d6db121
Chakri_34 <120355431+Chakri3434@users.noreply.github.com>
2024-05-26 16:07:10 +0530

    Update pattern-searching.py

--- Basic programs/Strings/pattern-searching.py
+++ Basic programs/Strings/pattern-searching.py
@@ -1,3 +1,4 @@
+#Naive :
 def f(s,p):
     n=len(s)
     k=len(p)
@@ -5,3 +6,20 @@ def f(s,p):
         if s[i:i+k]==p:
             print(i)
 f('geeksforgeeks','eks')
+
+#Improved Naive for distinct characters:
+def f(s,p):
+    n=len(s)
+    k=len(p)
+    i=0
+    while i<=n-k:
+        for j in range(k):
+            if p[j]!=s[i+j]:
+                break
+        if j==k:
+            print(i)
+        if j==0:
+            i+=1 
+        else:
+            i+=j
+f('abcabcd','abcd')
