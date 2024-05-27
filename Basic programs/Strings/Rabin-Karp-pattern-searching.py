aba0bdae05274c5dae7033f0b4e4ea644cf70f88
Chakri_34 <120355431+Chakri3434@users.noreply.github.com>
2024-05-27 11:16:09 +0530

    Create Rabin-Karp-pattern-searching.py

---
+++ Basic programs/Strings/Rabin-Karp-pattern-searching.py
@@ -0,0 +1,33 @@
+def f(txt,pat):
+    n=len(txt)
+    m=len(pat)
+    p,t,h=0,0,1
+    d=5
+    q=33
+
+    #calculate (d^(m-1))%q
+    for i in range(1,m):
+        h=(h*d)%q
+      
+    #calculate p and t0
+    for i in range(m):
+        p=(p*d + ord(pat[i]))%q
+        t=(t*d + ord(txt[i]))%q
+
+    for i in range(n-m):
+        #check for spurious hits
+        if p==t:
+            flag=True
+            for j in range(m):
+                if txt[i+j]!=pat[i]:
+                    flag=False
+                    break
+            if flag:
+                print(i)
+        #roll the hash
+        if i<(n-m):
+            t=((d*(t-ord(txt[i])*h))+ord(txt[i+m]))%q
+            if t<q:
+                t+=q
+    
+print(f('abcabcd','abcd'))
