b9e536d1c7dd2ad7340656f9e5ba06c231ed9864
Chakri_34 <120355431+Chakri3434@users.noreply.github.com>
2024-06-01 11:37:34 +0530

    Create Double-linked-list.py

---
+++ Basic programs/Linked Lists/Double-linked-list.py
@@ -0,0 +1,50 @@
+class Node:
+    
+    def __init__(self,data):
+        self.data = data
+        self.prev = None
+        self.next = None
+        
+class DoubleLinkedList:
+    
+    def __init__(self):
+        self.head = None
+        
+    def printLL(self):
+        curr = self.head
+        while curr != None:
+            print(curr.data)
+            curr = curr.next
+    
+    def insertBegin(self,data):
+        newnode = Node(data)
+        if self.head is None:
+            newnode.prev = None
+            self.head = newnode
+        else:
+            self.head.prev = newnode
+            newnode.next = self.head
+            self.head = newnode
+            newnode.prev = None
+        
+    def insertEnd(self,data):
+        newnode = Node(data)
+        if self.head is None:
+            newnode.prev = None
+            self.head = newnode
+        else:
+            curr = self.head
+            while curr.next != None:
+                curr = curr.next
+            curr.next = newnode
+            newnode.prev = curr
+            newnode.next = None
+
+dl1=DoubleLinkedList()
+dl1.insertBegin(30)
+dl1.insertBegin(40)
+dl1.insertEnd(50)
+dl1.insertBegin(20)
+dl1.insertEnd(10)
+dl1.printLL()
+        
