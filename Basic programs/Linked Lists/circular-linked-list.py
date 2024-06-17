0e9e1e0fcad5fe337029f611cd69cde1adb243f8
Chakri_34 <120355431+Chakri3434@users.noreply.github.com>
2024-06-17 11:43:51 +0530

    Update circular-linked-list.py

--- Basic programs/Linked Lists/circular-linked-list.py
+++ Basic programs/Linked Lists/circular-linked-list.py
@@ -39,16 +39,39 @@ class CircularLinkedList:
             curr.next = newnode
             newnode.next = self.head
     def deleteBegin(self):
-        temp = self.head.next
-        if temp == self.head:
-            self.head = None
+        if self.head is None:
+            print('Linked list is empty')
         else:
+            temp = self.head.next
+            if temp == self.head:
+                self.head = None
+            else:
+                curr = self.head
+                while curr.next != self.head:
+                    curr = curr.next
+                curr.next = temp
+                self.head = temp
+    def DeleteKth(self,pos):
+        if pos==1:
+            if self.head is None:
+                print('Linked list is empty')
+            else:
+                temp = self.head.next
+                if temp == self.head:
+                    self.head = None
+                else:
+                    curr = self.head
+                    while curr.next != self.head:
+                        curr = curr.next
+                    curr.next = temp
+                    self.head = temp
+        else:
+            c=1
             curr = self.head
-            while curr.next != self.head:
+            while c != pos-1:
                 curr = curr.next
-            curr.next = temp
-            self.head = temp
-        
+                c+=1
+            curr.next = curr.next.next
 cl=CircularLinkedList()
 cl.addBegin(10)
 cl.addEnd(60)
@@ -56,4 +79,5 @@ cl.addBegin(20)
 cl.addBegin(-30)
 cl.addEnd(40)
 cl.deleteBegin()
+cl.DeleteKth(3)
 cl.traverse()
