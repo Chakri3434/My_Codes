7b3eb0ccf0bc391ecdd26f645b6201fdd1f08be8
Chakri_34 <120355431+Chakri3434@users.noreply.github.com>
2024-06-21 11:25:00 +0530

    Update Single-LL.py

--- Basic programs/Linked Lists/Single-LL.py
+++ Basic programs/Linked Lists/Single-LL.py
@@ -14,7 +14,7 @@ class LinkedList:
             print('The linked list is empty')
         else:
             while self.head.next != None:
-                print(self.head.data,'-->',end='')
+                print(self.head.data,'--> ',end='')
                 self.head = self.head.next
             print(self.head.data)    
     def insertBegin(self,data):
@@ -51,25 +51,70 @@ class LinkedList:
         else:
             newnode.next = curr
             self.head = newnode
+            
+    def insertSorted(self,data):
+        if self.head is None:
+            return -1 
+        curr = self.head
+        position = 1
+        while curr and data > curr.data:
+            curr = curr.next
+            position+=1
+        return self.insertNode(data,position)
+            
+    def middleofLL(self):
+        if self.head is None:
+            return -1
+        slow=self.head
+        fast=self.head
+        while fast!=None and fast.next!=None:
+            slow = slow.next
+            fast = fast.next.next
+        print(slow.data)
+    """
+    NAIVE METHOD
     
-    def search(self,data):
-        curr=self.head
-        c=1
-        while curr != None:
-            if curr.data == data:
-                return c
-            else:
-                c+=1
-                curr = curr.next
-        return -1
+    def NthNodefromEnd(self,n):
+        curr = self.head
+        length=1
+        while curr.next != None:
+            length+=1
+            curr = curr.next
+        curr = self.head
+        if length < n :
+            return 
+        for i in range(length-n):
+            curr = curr.next
+        print(curr.data)
+    """
+    def NthNodefromEnd(self,n):
+        if self.head is None:
+            return -1
+        left = self.head
+        right = self.head
+        for i in range(n):
+            right = right.next
+        while right != None:
+            right = right.next
+            left = left.next
+        print(left.data)
+        
+    def deleteIndex(self,index):
+        curr = self.head
+        for i in range(index-2):
+            curr = curr.next
+        curr.next = curr.next.next
 l1=LinkedList()
-l1.insertBegin(10)
-l1.insertEnd(40)
-l1.insertBegin(20)
-l1.insertEnd(30)
+l1.insertBegin(60)
 l1.insertBegin(50)
-l1.insertEnd(60)
-l1.deleteBegin()
-l1.insertNode(50,3)
-print(l1.search(70))
+l1.insertBegin(40)
+l1.insertBegin(30)
+l1.insertBegin(20)
+l1.insertBegin(10)
+l1.insertSorted(65)
+l1.NthNodefromEnd(5)
+l1.middleofLL()
+l1.deleteIndex(3)
 l1.traverse()
+
+        
