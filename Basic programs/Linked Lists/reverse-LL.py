bbce59909c7c6687dd0a9ed3f7465c160173463f
Chakri_34 <120355431+Chakri3434@users.noreply.github.com>
2024-06-21 11:34:44 +0530

    Update reverse-LL.py

--- Basic programs/Linked Lists/reverse-LL.py
+++ Basic programs/Linked Lists/reverse-LL.py
@@ -21,3 +21,17 @@ class Solution:
             root.next = ListNode(s[i])
             root = root.next
         return call.next
+
+## EFFICIENT ##
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return
+        curr = head
+        prev = None
+        while curr:
+            nextt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nextt
+        return prev
