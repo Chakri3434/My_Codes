946aedc24bac7cc722fb0b1f5bd4ae5e8ae9e6d0
Chakri_34 <120355431+Chakri3434@users.noreply.github.com>
2024-06-23 09:52:42 +0530

    Update remove-duplicates-(self also).py

--- Basic programs/Linked Lists/remove-duplicates-(self also).py
+++ Basic programs/Linked Lists/remove-duplicates-(self also).py
@@ -17,3 +17,34 @@ class Solution:
                 pre = pre.next
                 head = head.next
         return dummy.next
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        d={}
+        curr = head
+        while curr:
+            if curr.val in d:
+                d[curr.val]+=1
+            else:
+                d[curr.val]=1
+            curr = curr.next
+        s=set()
+        for k,v in d.items():
+            if v>1:
+                s.add(k)
+        print(s)
+        dummy = pre = ListNode(0)
+        dummy.next = head
+        while dummy and dummy.next:
+            if dummy.next.val in s:
+                dummy.next = dummy.next.next
+            else:
+                dummy = dummy.next
+        return pre.next
+        
