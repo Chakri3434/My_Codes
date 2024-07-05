2a03db7f527132ccf6ccbe67dce17b18f2d3c7db
Chakri_34 <120355431+Chakri3434@users.noreply.github.com>
2024-07-05 16:13:23 +0530

    Update largest-rectangle-histogram.py

--- Basic programs/Stack/largest-rectangle-histogram.py
+++ Basic programs/Stack/largest-rectangle-histogram.py
@@ -21,3 +21,43 @@ class Solution:
                     break
             res=max(res,curr)
         return res
+
+# Better O(n)
+
+class Solution:
+    def largestRectangleArea(self, nums: List[int]) -> int:
+        ps=[]
+        ps.append(-1)
+        istack=[]
+        istack.append(0)
+        for i in range(1,len(nums)):
+            while istack and nums[istack[-1]]>=nums[i]:
+                istack.pop()
+            if not istack:
+                ps.append(-1)
+            else:
+                ps.append(istack[-1])
+            istack.append(i)
+        istack=[]
+        ns=[0]*len(nums)
+        ns[-1]=len(nums)
+        i = len(nums)-2
+        istack.append(len(nums)-1)
+        while i>=0:
+            while istack and nums[istack[-1]]>=nums[i]:
+                istack.pop()
+            if not istack:
+                ns[i]=len(nums)
+            else:
+                ns[i]=istack[-1]
+            istack.append(i)
+            i-=1 
+        print(ns)
+        res=0
+        for i in range(len(nums)):
+            curr=nums[i]
+            curr+=(i-ps[i]-1)*nums[i]
+            curr+=(ns[i]-i-1)*nums[i]
+            res=max(res,curr)
+        return res
+
