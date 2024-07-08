31a7f3c700959efcb70dfd00b1556b7b22b69966
Chakri_34 <120355431+Chakri3434@users.noreply.github.com>
2024-07-08 10:47:37 +0530

    Update joshephus-problem.py

--- Basic programs/Recursions/joshephus-problem.py
+++ Basic programs/Recursions/joshephus-problem.py
@@ -1,3 +1,19 @@
+# NAIVE O(n*k)
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        arr = [i for i in range(1,n+1)]
+        s=0
+        while len(arr)!=1:
+            c=1 
+            while c!=k:
+                s+=1
+                s=s%len(arr)
+                c+=1 
+            arr.pop(s)
+            print(arr)
+        return arr[0]
+
+
 def jos(n,k):
   if n==1:
     return 0
