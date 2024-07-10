1df1715a2b03b02d2a31d478cc2152596472bdfb
Chakri_34 <120355431+Chakri3434@users.noreply.github.com>
2024-07-10 15:16:24 +0530

    Update design-getMin-stack.py

--- Basic programs/Stack/design-getMin-stack.py
+++ Basic programs/Stack/design-getMin-stack.py
@@ -32,3 +32,42 @@ l.push(1)
 l.popp()
 l.popp()
 l.getMin()
+
+
+#  O(1) Space
+class MinStack:
+
+    def __init__(self):
+        self.s=[]
+        self.min=0
+
+    def push(self, val: int) -> None:
+        if self.s==[]:
+            self.min=val
+            self.s.append(val)
+        elif val<=self.min:
+            self.s.append(2*val-self.min)
+            self.min = val
+        else:
+            self.s.append(val)
+
+    def pop(self) -> None:
+        if self.s[-1]<=self.min:
+            self.min=2*self.min-self.s[-1]
+        self.s.pop()
+
+    def top(self) -> int:
+        if self.s[-1]<=self.min:
+            return self.min
+        return self.s[-1]
+
+    def getMin(self) -> int:
+        return self.min
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
