9c3d6cb9d51856f734a06caaec0844280703b17e
Chakri_34 <120355431+Chakri3434@users.noreply.github.com>
2024-07-12 11:32:08 +0530

    Create reverse-queue.py

---
+++ Basic programs/Queue/reverse-queue.py
@@ -0,0 +1,17 @@
+from collections import deque
+q=deque()
+q.append(20)
+q.append(5)
+q.append(12)
+q.append(6)
+print(q)
+
+def rev(q):
+    if len(q)==0:
+        return
+    x=q[0]
+    q.popleft()
+    rev(q)
+    q.append(x)
+rev(q)
+print(q)
