0473dd11420067e40c73412d622a667451371a83
Chakri_34 <120355431+Chakri3434@users.noreply.github.com>
2024-07-16 16:13:46 +0530

    Update height-of-binary-tree.py

--- Basic programs/Trees/height-of-binary-tree.py
+++ Basic programs/Trees/height-of-binary-tree.py
@@ -20,3 +20,17 @@ class Solution:
                     st.append(node.right)
             c+=1
         return c
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root==None:
+            return 0
+        return 1+max(self.maxDepth(root.left),self.maxDepth(root.right))
+
