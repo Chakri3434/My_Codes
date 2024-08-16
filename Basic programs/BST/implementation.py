05c197a806b4640ad1d1f469443aec31d7570455
Chakri_34 <120355431+Chakri3434@users.noreply.github.com>
2024-08-16 07:42:59 +0530

    Update implementation.py

--- Basic programs/BST/implementation.py
+++ Basic programs/BST/implementation.py
@@ -1,28 +1,48 @@
-class TreeNode:
-    def __init__(self,value):
-        self.val=value
-        self.left=None
-        self.right=None
-    def insert(self,value):
-        if value<self.val:
-            if self.left:
-                self.left.insert(value)
-            else:
-                self.left=TreeNode(value)
+class Tree:
+    def __init__(self,val=None):
+        self.value = val
+        if self.value:
+            self.left = Tree()
+            self.right = Tree()
         else:
-            if self.right:
-                self.right.insert(value)
-            else:
-                self.right=TreeNode(value)
+            self.left = None
+            self.right = None
+    def isempty(self):
+        return (self.value == None)
+    def insert(self,data):
+        if self.isempty():
+            self.value = data
+            self.left = Tree()
+            self.right = Tree()
+        elif data < self.value:
+            self.left.insert(data)
+            return
+        elif data > self.value:
+            self.right.insert(data)
+        elif data == self.value:
+            return
     def inorder(self):
-        if self.left:self.left.inorder()
-        print(self.val)
-        if self.right:self.right.inorder()
-tree=TreeNode(50)
+        if self.isempty():
+            return []
+        else:
+            return self.left.inorder() + [self.value] + self.right.inorder()
+    def search(self,data):
+        if self.isempty():
+            print('Not Found')
+            return
+        if data==self.value:
+            print('Found')
+            return
+        if data<self.value:
+            self.left.search(data)
+        else:
+            self.right.search(data)
+tree=Tree(50)
 tree.insert(30)
 tree.insert(10)
 tree.insert(40)
 tree.insert(70)
 tree.insert(60)
 tree.insert(80)
-tree.inorder()
+print(tree.inorder())
+tree.search(90)
